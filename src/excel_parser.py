f437d0668c5445a2144b4cfa28f6b75eae4fc8c0
Doksam <camjedi99@gmail.com>
2024-11-01 14:09:21 -0700

    new data formed

--- src/excel_parser.py
+++ src/excel_parser.py
@@ -3,7 +3,7 @@ import openpyxl
 import csv
 
 #directory for excel files
-directory = "first_round_2024"
+directory = "C:/Users/cameronshaw/Documents/Affordable Research/All Applications Since 2022/first_round_2024"
 #gets the excel files
 files = os.listdir(directory)
 #turns them into usable filepaths for method
@@ -34,14 +34,19 @@ def get_CTCHC_data(file_path):
     #     property_manager_contact
     # ]
 
-    application_data = ['AG437', 'AG442', 'O773', 'T773', 'AG993', 'AG449']
+    application_data = [
+        'AG437', 'AG442', 'O773', 'T773', 'AG993', 'AG449', 'AD411', 'AE424',
+        'AA287', 'Q494', 'S413'
+    ]
+
+    address = ['I185']
 
     budget_and_sources = [
         'B12', 'B26', 'B38', 'B42', 'B43', 'B54', 'B62', 'B70', 'B77', 'B79',
         'B80', 'B96', 'B104', 'B13', 'B14', 'B27'
     ]
 
-    cells_to_search = budget_and_sources
+    cells_to_search = address
 
     #load excel file
     workbook = openpyxl.load_workbook(file_path, data_only=True)
@@ -49,7 +54,7 @@ def get_CTCHC_data(file_path):
     #Select the sheet
     app = "Application"
     budg = 'Sources and Uses Budget'
-    sheet = workbook[budg]
+    sheet = workbook[app]
 
     #get the values
     cell_data = [sheet[i].value for i in cells_to_search]
@@ -60,28 +65,35 @@ def get_CTCHC_data(file_path):
             cell_data[j] = 'None'
 
     #per value changes needed
-    if isinstance(cell_data[1], str) or isinstance(
-            cell_data[2], str) or isinstance(cell_data[3], str):
-        cell_data[1] = 0
-        cell_data[2] = 0
-        cell_data[3] = 0
-        print(f'skipped numbers for this:{file_path}')
+    # if isinstance(cell_data[1], str) or isinstance(
+    #         cell_data[2], str) or isinstance(cell_data[3], str):
+    #     cell_data[1] = 0
+    #     cell_data[2] = 0
+    #     cell_data[3] = 0
+    #     print(f'skipped numbers for this:{file_path}')
 
-    land_cost = cell_data[0] + cell_data[13] + cell_data[14]
-    hard_cost = cell_data[1] + cell_data[2] + cell_data[9] + cell_data[15]
-    soft_cost = cell_data[7] + cell_data[8] + cell_data[10] + cell_data[11]
-    architectural_cost = cell_data[3] + cell_data[4]
-    finance_cost = cell_data[5] + cell_data[6]
-    developer_fee = cell_data[12]
+    # land_cost = cell_data[0] + cell_data[13] + cell_data[14]
+    # hard_cost = cell_data[1] + cell_data[2] + cell_data[9] + cell_data[15]
+    # soft_cost = cell_data[7] + cell_data[8] + cell_data[10] + cell_data[11]
+    # architectural_cost = cell_data[3] + cell_data[4]
+    # finance_cost = cell_data[5] + cell_data[6]
+    # developer_fee = cell_data[12]
 
     #per value changes needed
 
     print(f'returning started:{file_path}')
     to_return = [
-        land_cost, hard_cost, soft_cost, architectural_cost, finance_cost,
-        developer_fee
-        # cell_data[0], cell_data[1] + cell_data[2] * cell_data[3], cell_data[4],
-        # cell_data[5]
+        # land_cost, hard_cost, soft_cost, architectural_cost, finance_cost,
+        # developer_fee
+        # cell_data[0],
+        # cell_data[1] + cell_data[2] * cell_data[3],
+        # cell_data[4],
+        # cell_data[5],
+        # cell_data[6],
+        # cell_data[7],
+        # cell_data[8],
+        # cell_data[9],
+        # cell_data[10]
 
         # cell_data[0], cell_data[1], cell_data[2],
         # cell_data[3] + ' - ' + cell_data[4],
@@ -90,6 +102,7 @@ def get_CTCHC_data(file_path):
         # cell_data[9] + ' - ' + cell_data[10],
         # cell_data[11] + ' - ' + cell_data[12],
         # cell_data[13] + ' - ' + cell_data[14]
+        cell_data[0]
     ]
     print(f'data returned:{file_path}')
     return to_return
@@ -98,11 +111,14 @@ def get_CTCHC_data(file_path):
 data = [get_CTCHC_data(i) for i in excel_paths]
 
 #writes excel spreadsheet data into a csv file
-with open('CTCHC2.csv', 'w', newline='') as file:
+with open('CTCHC3.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    app = ['Units', 'NRSF', 'Prevailing Wage', 'Parking SF']
+    app = [
+        'Units', 'NRSF', 'Prevailing Wage', 'Parking SF', 'Stories',
+        "Buildings", 'Architect', 'Parking Spaces', 'Subterranean?'
+    ]
     budg = ['land', 'hard', 'soft', 'arch', 'finance', 'dev']
-    fields = budg
+    fields = app
 
     writer.writerow(fields)
     writer.writerows(data)
