a5b5a60ae68858d8a3c29e71d645632bc9e60710
Doksam <camjedi99@gmail.com>
2024-11-11 09:19:34 -0800

    more options

--- src/excel_parser.py
+++ src/excel_parser.py
@@ -34,6 +34,8 @@ def get_CTCHC_data(file_path):
     #     property_manager_contact
     # ]
 
+    app_number = "CA-" + file_path.split('/')[7].split('.')[0]
+
     application_data = [
         'AG437', 'AG442', 'O773', 'T773', 'AG993', 'AG449', 'AD411', 'AE424',
         'AA287', 'Q494', 'S413'
@@ -46,7 +48,35 @@ def get_CTCHC_data(file_path):
         'B80', 'B96', 'B104', 'B13', 'B14', 'B27'
     ]
 
-    cells_to_search = address
+    construction_financing = [['C554', 'M554', 'Q554', 'W554', 'AM554'],
+                              ['C555', 'M555', 'Q555', 'W555', 'AM555'],
+                              ['C556', 'M556', 'Q556', 'W556', 'AM556'],
+                              ['C557', 'M557', 'Q557', 'W557', 'AM557'],
+                              ['C558', 'M558', 'Q558', 'W558', 'AM558'],
+                              ['C559', 'M559', 'Q559', 'W559', 'AM559'],
+                              ['C560', 'M560', 'Q560', 'W560', 'AM560'],
+                              ['C561', 'M561', 'Q561', 'W561', 'AM561'],
+                              ['C562', 'M562', 'Q562', 'W562', 'AM562'],
+                              ['C563', 'M563', 'Q563', 'W563', 'AM563'],
+                              ['C564', 'M564', 'Q564', 'W564', 'AM564'],
+                              ['C565', 'M565', 'Q565', 'W565', 'AM565']]
+
+    permanent_financing = [['C627', 'M627', 'Q627', 'W627', 'AO627'],
+                           ['C628', 'M628', 'Q628', 'W628', 'AO628'],
+                           ['C629', 'M629', 'Q629', 'W629', 'AO629'],
+                           ['C630', 'M630', 'Q630', 'W630', 'AO630'],
+                           ['C631', 'M631', 'Q631', 'W631', 'AO631'],
+                           ['C632', 'M632', 'Q632', 'W632', 'AO632'],
+                           ['C633', 'M633', 'Q633', 'W633', 'AO633'],
+                           ['C634', 'M634', 'Q634', 'W634', 'AO634'],
+                           ['C635', 'M635', 'Q635', 'W635', 'AO635'],
+                           ['C636', 'M636', 'Q636', 'W636', 'AO636'],
+                           ['C637', 'M637', 'Q637', 'W637', 'AO637'],
+                           ['C638', 'M638', 'Q638', 'W638', 'AO638']]
+
+    parking_spaces = ['Q494', 'M495', 'AH495']
+
+    cells_to_search = ['AD411', 'AD412']
 
     #load excel file
     workbook = openpyxl.load_workbook(file_path, data_only=True)
@@ -59,10 +89,24 @@ def get_CTCHC_data(file_path):
     #get the values
     cell_data = [sheet[i].value for i in cells_to_search]
 
+    #for getting financial data
+    # cell_data = []
+    # for row in cells_to_search:
+    #     lender = sheet[row[0]].value
+    #     source = sheet[row[1]].value
+    #     term = sheet[row[2]].value
+    #     interest = sheet[row[3]].value
+    #     funds = sheet[row[4]].value
+    #     cell_data.append([app_number, lender, source, term, interest, funds])
+
     #removes null values
-    for j in range(len(cell_data)):
-        if cell_data[j] is None:
-            cell_data[j] = 'None'
+    # for j in range(len(cell_data)):
+    #     if cell_data[j] is None:
+    #         cell_data[j] = 'None'
+
+    # for j in range(len(cell_data) - 1, -1, -1):
+    #     if cell_data[j][5] is None:
+    #         cell_data.pop(j)
 
     #per value changes needed
     # if isinstance(cell_data[1], str) or isinstance(
@@ -83,27 +127,39 @@ def get_CTCHC_data(file_path):
 
     print(f'returning started:{file_path}')
     to_return = [
-        # land_cost, hard_cost, soft_cost, architectural_cost, finance_cost,
-        # developer_fee
-        # cell_data[0],
-        # cell_data[1] + cell_data[2] * cell_data[3],
-        # cell_data[4],
-        # cell_data[5],
-        # cell_data[6],
-        # cell_data[7],
-        # cell_data[8],
-        # cell_data[9],
-        # cell_data[10]
+        app_number,
+        #     # land_cost, hard_cost, soft_cost, architectural_cost, finance_cost,
+        #     # developer_fee
+        #     # cell_data[0],
+        #     # cell_data[1] + cell_data[2] * cell_data[3],
+        #     # cell_data[4],
+        #     # cell_data[5],
+        #     # cell_data[6],
+        #     # cell_data[7],
+        #     # cell_data[8],
+        #     # cell_data[9],
+        #     # cell_data[10]
 
-        # cell_data[0], cell_data[1], cell_data[2],
-        # cell_data[3] + ' - ' + cell_data[4],
-        # cell_data[5] + ' - ' + cell_data[6],
-        # cell_data[7] + ' - ' + cell_data[8],
-        # cell_data[9] + ' - ' + cell_data[10],
-        # cell_data[11] + ' - ' + cell_data[12],
-        # cell_data[13] + ' - ' + cell_data[14]
-        cell_data[0]
+        #   various budget data
+        #     # cell_data[0], cell_data[1], cell_data[2],
+        #     # cell_data[3] + ' - ' + cell_data[4],
+        #     # cell_data[5] + ' - ' + cell_data[6],
+        #     # cell_data[7] + ' - ' + cell_data[8],
+        #     # cell_data[9] + ' - ' + cell_data[10],
+        #     # cell_data[11] + ' - ' + cell_data[12],
+        #     # cell_data[13] + ' - ' + cell_data[14]
+        #     cell_data[0]
+
+        # parking data
+        # cell_data[0],
+        # cell_data[1],
+        # cell_data[2]
+
+        #single data point
+        cell_data[0],
+        cell_data[1]
     ]
+
     print(f'data returned:{file_path}')
     return to_return
 
@@ -111,14 +167,19 @@ def get_CTCHC_data(file_path):
 data = [get_CTCHC_data(i) for i in excel_paths]
 
 #writes excel spreadsheet data into a csv file
-with open('CTCHC3.csv', 'w', newline='') as file:
+with open('stories.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     app = [
         'Units', 'NRSF', 'Prevailing Wage', 'Parking SF', 'Stories',
         "Buildings", 'Architect', 'Parking Spaces', 'Subterranean?'
     ]
     budg = ['land', 'hard', 'soft', 'arch', 'finance', 'dev']
+
     fields = app
 
     writer.writerow(fields)
+    # for writing a list of lists
+    # for project in data:
+    #     writer.writerows(project)
+    # for writing a single table
     writer.writerows(data)
